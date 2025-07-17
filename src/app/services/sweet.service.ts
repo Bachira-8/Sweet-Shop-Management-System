9e5f140fee9c0ebb1508d76f870d8c616c063dc7
Dhruv <dhruvvasava666@gmail.com>
2025-07-17 19:24:50 +0530

    Implement add sweet feature

--- src/app/services/sweet.service.ts
+++ src/app/services/sweet.service.ts
@@ -8,5 +8,19 @@ export class SweetService {
 
   // constructor() { }
 
-  
+  private sweets: Sweet[] = [
+    { id: 1, name: 'Kaju Katli', category: 'Dry Fruit', price: 50 },
+    { id: 2, name: 'Rasgulla', category: 'Syrup', price: 25 },
+
+  ];
+
+  getSweets(): Sweet[] {
+    return this.sweets;
+  }
+
+  addSweet(sweet: Sweet) {
+    sweet.id = this.sweets.length + 1;
+    this.sweets.push(sweet);
+  }
+
 }
